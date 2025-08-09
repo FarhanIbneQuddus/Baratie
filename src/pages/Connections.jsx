0133315cfd8586fd517c48d9021c855409fd00d1
Farhan Fahim <1.fahimcoc@gmail.com>
2025-08-09 07:11:17 +0600

    Added Discover section

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -22,8 +22,8 @@ const Connections = () => {
   ]
 
   return (
-    <div className='min-h-screen bg-slate-50'>
-      <div className='max-w-6xl mx-auto p-6'>
+    <div className='min-h-screen bg-gradient-to-r from-yellow-100 to-blue-500'>
+      <div className='max-w-6xl mx-auto pl-20 pt-20'>
 
         {/* Title */}
         <div className='mb-8'>
@@ -33,7 +33,7 @@ const Connections = () => {
         {/* Counts */}
         <div className='mb-8 flex flex-warp gap-6'>
           {dataArray.map((item, index)=>(
-            <div key={index} className='flex flex-col items-center justify-center gap-1 border h-20 w-40 border-gray-200 bg-white shadow rounded-md'>
+            <div key={index} className='flex flex-col items-center justify-center gap-1 border h-20 w-40 border-gray-200 bg-gradient-to-r from-indigo-200 to-blue-100 shadow rounded-md'>
               <b>{item.value.length}</b>
               <p className='text-slate-600'>{item.label}</p>
             </div>
@@ -41,10 +41,10 @@ const Connections = () => {
         </div>
 
         {/* Tabs */}
-        <div className='inline-flex flex-wrap items-center border border-gray-200 rounded-md p-1 bg-white shadow-sm'>
+        <div className='inline-flex flex-wrap items-center border border-gray-200 rounded-md p-1 bg-gradient-to-r from-indigo-200 to-blue-100 shadow-sm'>
           {
             dataArray.map((tab)=>(
-              <button onClick={()=> setCurrentTab(tab.label)} key={tab.label} className={`cursor-pointer flex items-center px-3 py-1 text-sm rounded-md transition-colors ${currentTab === tab.label ? 'bg-white font-medium text-black' : 'text-gray-500 hover:text-black'}`}>
+              <button onClick={()=> setCurrentTab(tab.label)} key={tab.label} className={`cursor-pointer flex items-center px-3 py-1 text-sm rounded-md transition-colors ${currentTab === tab.label ? 'bg-gradient-to-r from-indigo-400 to-blue-300 font-medium text-black' : 'text-gray-500 hover:text-black'}`}>
                 <tab.icon className='w-4 h-4'/>
                 <span className='ml-1'>{tab.label}</span>
                 {tab.count !== undefined && (
@@ -58,7 +58,7 @@ const Connections = () => {
         {/* Connections */}
         <div className='flex flex-wrap gap-6 mt-6'>
           {dataArray.find((item)=>item.label === currentTab).value.map((user)=>(
-            <div key={user._id} className='w-full max-w-88 flex gap-5 p-6 bg-white shadow rounded-mb'>
+            <div key={user._id} className='w-full max-w-88 flex gap-5 p-6 bg-gradient-to-r from-indigo-100 to-blue-300 shadow rounded-mb'>
               <img src={user.profile_picture} alt="" className="rounded-full w-12 h-12 shadow-md mx-auto" />
               <div className='flex-1'>
                 <p className='font-medium text-slate-700'>{user.full_name}</p>
