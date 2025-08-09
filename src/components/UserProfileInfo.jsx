071c38479414fddeb5e0186814f4570e80f2a813
Farhan Fahim <1.fahimcoc@gmail.com>
2025-08-09 15:02:27 +0600

    Added Profile Section

---
+++ src/components/UserProfileInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserProfileInfo = ({user, posts, profileId, setShowEdit}) => {
+  return (
+    <div className='relative py-4 px-6 md:px-8 bg-white'>
+      
+    </div>
+  )
+}
+
+export default UserProfileInfo
